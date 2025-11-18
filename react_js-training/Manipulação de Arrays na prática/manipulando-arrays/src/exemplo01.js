579a1edce166988754105003ed497e1dcc6c18f9
Esdras Lira <122489986+Eynng@users.noreply.github.com>
2025-11-18 12:11:34 -0300

    LÓGICA COM REACT: MANIPULAÇÃO DE ARRAYS NA PRÁTICA (Imutabilidade e Métodos Essenciais) Part 1

---
+++ react_js-training/Manipulação de Arrays na prática/manipulando-arrays/src/exemplo01.js
@@ -0,0 +1,5 @@
+const numeros = [1,2,3,4];
+
+numeros.push(100);
+
+console.log(numeros)
---
+++ react_js-training/Manipulação de Arrays na prática/manipulando-arrays/src/exemplo02.js
@@ -0,0 +1,10 @@
+const lista = [1, 2, 3, 4];
+const numeros = [...lista];
+
+console.log(numeros);
+console.log(lista);
+
+numeros.push(99);
+
+console.log(numeros);
+console.log(lista);
